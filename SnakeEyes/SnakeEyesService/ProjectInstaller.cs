0830834c8ddf595fd4aa851e2ce4c0346e320b26
Rolf Kristensen <sweaty1@hotmail.com>
2017-04-27 20:22:42 +0200

    Improved service-installer to handle path with spaces.

--- SnakeEyes/SnakeEyesService/ProjectInstaller.cs
+++ SnakeEyes/SnakeEyesService/ProjectInstaller.cs
@@ -14,6 +14,16 @@ namespace SnakeEyes
             InitializeComponent();
         }
 
+        protected virtual string AppendPathParameter(string path, string parameter)
+        {
+            if (path.Length > 0 && path[0] != '"')
+            {
+                path = "\"" + path + "\"";
+            }
+            path += " " + parameter;
+            return path;
+        }
+
         protected override void OnBeforeInstall(System.Collections.IDictionary savedState)
         {
             // Apply the ServiceDependencies specified in the app.config file
@@ -22,17 +32,19 @@ namespace SnakeEyes
             Configuration config = ConfigurationManager.OpenExeConfiguration(assemblyPath);
 
             KeyValueConfigurationElement dependencies = config.AppSettings.Settings["ServiceDependencies"];
-            if (dependencies != null)
+            if (dependencies != null && !string.IsNullOrEmpty(dependencies.Value))
             {
                 serviceInstaller1.ServicesDependedOn = dependencies.Value.Split(',');
             }
-            Context.Parameters["assemblypath"] += "\" + /SERVICE";
+
+            Context.Parameters["assemblypath"] = AppendPathParameter(assemblyPath, "/service");
             base.OnBeforeInstall(savedState);
         }
 
         protected override void OnBeforeUninstall(System.Collections.IDictionary savedState)
         {
-            Context.Parameters["assemblypath"] += "\" /SERVICE";
+            string assemblyPath = Context.Parameters["assemblypath"];
+            Context.Parameters["assemblypath"] = AppendPathParameter(assemblyPath, "/service");
             base.OnBeforeUninstall(savedState);
         }
     }
