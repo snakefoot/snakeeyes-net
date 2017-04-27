ace561f4aea3349d23b1f0cc150900b3f02faffe
Jakob Sack <mail@jakobsack.de>
2017-04-27 16:24:05 +0200

    Fix the installer if no value for "ServiceDependencies" is given.

--- SnakeEyes/SnakeEyesService/ProjectInstaller.cs
+++ SnakeEyes/SnakeEyesService/ProjectInstaller.cs
@@ -20,10 +20,12 @@ namespace SnakeEyes
             // http://raquila.com/software/configure-app-config-application-settings-during-msi-install/
             string assemblyPath = Context.Parameters["assemblypath"];
             Configuration config = ConfigurationManager.OpenExeConfiguration(assemblyPath);
-            string dependencies = config.AppSettings.Settings["ServiceDependencies"].Value;
-            if (dependencies != null)
-                serviceInstaller1.ServicesDependedOn = dependencies.Split(',');
 
+            KeyValueConfigurationElement dependencies = config.AppSettings.Settings["ServiceDependencies"];
+            if (dependencies != null)
+            {
+                serviceInstaller1.ServicesDependedOn = dependencies.Value.Split(',');
+            }
             Context.Parameters["assemblypath"] += "\" + /SERVICE";
             base.OnBeforeInstall(savedState);
         }
